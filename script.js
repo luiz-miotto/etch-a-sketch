09be796259185cb8f4582c7d26921f8b3ed7eede
Luiz-Miotto <luiz.l.miotto94@gmail.com>
2022-05-08 15:06:01 -0400

    added logic for changing single div color

--- script.js
+++ script.js
@@ -12,31 +12,69 @@ const ids = Array.from(Array(256).keys());
             
         </div>
      `;
- let changeColor = document.getElementById(e);
-
- 
- changeColor.addEventListener("mouseover", () => {
-    console.log(`the id is ${e}`);
-    return document.getElementById(e).style.color.red;
-
-});
-
- 
- 
- /*changeColor.addEventListener("mouseenter", (e) => {
-     document.getElementById(e).style.color="red";
-
- });*/
 
 
  });
 
- /*ids.forEach(function(e){
+
+
+ let changeColor = document.getElementById("255");
+
+ changeColor.addEventListener("mouseover", () => {
+    //console.log(`the id is ${e}`);
+     document.getElementById("255").style.backgroundColor= "red";
+ });
+
+
+ /*const changeColor = document.querySelector("255")
+
+ changeColor.addEventListener("mouseover", () => {
+    //console.log(`the id is ${e}`);
+     document.getElementById("255").style.backgroundColor= "red";
+ });
+
+*/
+/*
+let getId = document.getElementsByClassName("divClass")
+
+ getId.addEventListener("mouseover",()=>{
+     document.getElementById().style.backgroundColor="red";
+     
+ });
+*/
+ /*
+  changeColor.addEventListener("mouseover", function(e){
+    //console.log(`the id is ${e}`);
+     document.getElementById(e).style.backgroundColor= "red";
+ });
+
+ */   
+
+
+/*
+
+
+
+ids.forEach(function(e){
+  var changeColor = document.getElementById(e);
+});
+
+ const newColor = document.querySelectorAll
+ let changeColor = document.getElementByClass("divClass");
+
+document.getElementById()
+ 
+ changeColor.addEventListener("mouseover", () => {
+    //console.log(`the id is ${e}`);
+     document.getElementById("255").style.backgroundColor= "red";
+ });
+
+ ids.forEach(function(e){
      document.getElementById(`${e}`).style
 
      
  const colorChange = document.querySelector()
-/*
+
  var div = document.createElement('div');
  var divider = document.querySelector('div');
  divider.style.cssText = 'color:blue; background: red;';
